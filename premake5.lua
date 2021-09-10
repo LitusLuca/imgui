edebfdf5a92ea52a47b38cf787d62a5256d6c07a
Luca <wachters.luca@gmail.com>
2021-09-10 15:08:35 +0200

    edited premake5.lua and .gitignore

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,8 @@
 project "ImGui"
 	kind "StaticLib"
 	language "C++"
+    cppdialect "C++17"
+    staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
